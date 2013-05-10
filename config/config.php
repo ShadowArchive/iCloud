42187fcab7f0e43fd101ff3767f94eb2d54828f8
shenhongmings@gmail.com <shenhongmings@gmail.com>
2013-05-10 18:02:27 +0800

    Init iColund

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array (
+  'instanceid' => '5183324bca49c',
+  'passwordsalt' => 'jygvwb5gzy0of9da1evx1yur1b18xw',
+  'datadirectory' => 'C:\\webaccess\\V\\owncloud\\data',
+  'dbtype' => 'mysql',
+  'version' => '5.0.6',
+  'dbname' => 'codebase',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_zero',
+  'dbpassword' => '2848256ab134c652b90f6f8f7486615a',
+  'installed' => true,
+);
